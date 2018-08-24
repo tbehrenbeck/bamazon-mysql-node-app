23e337afc9391ad434d8f2e3422050508c010922
Tommie Behrenbeck <behrenbt@gmail.com>
2018-08-24 07:51:13 -0500

    storeUserChoices

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,40 +2,45 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost", 
+    host: "localhost",
     port: 8889,
-    user: "root", 
+    user: "root",
     password: "root",
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     //console.log("connected as id " + connection.threadId);
     start();
 });
 
-// first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// first display all of the items available for sale
 function start() {
     connection.query("SELECT * FROM products", function (err, res) {
         console.log("\nItems for SALE:" + "\nID--Product------------Price-");
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].id + " | "  + res[i].product_name + " | " + parseInt(res[i].price).toFixed(2)); 
+            console.log(res[i].id + " | " + res[i].product_name + " | " + parseInt(res[i].price).toFixed(2));
         }
         console.log("--------------------");
     });
 }
+wantToBuy();
 
-//The app should then prompt users with two messages.--ask them the ID of the product they would like to buy--how many units of the product they would like to buy.
 
+//The app should then prompt users with two messages
 function wantToBuy() {
-    inquirer
+
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer
         .prompt([
             {
+                name: "input",
                 type: "input",
-                name: "userChoice",
-                message: "What is the ID of the product you would like to buy?",
-                validate: function(value) {
+                message: "What is the ID of the product you would like to buy?", 
+                validate: function (value) {
                     if (isNaN(value) === false) {
                         return true;
                     }
@@ -43,7 +48,69 @@ function wantToBuy() {
                 }
             },
             {
-                
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to buy",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
             }
         ])
+        .then(function (answer) {
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if(results[i].product_name === answer.choice) {
+                    chosenItem = results[i];
+                }
+            }
+            console.log(chosenItem);
+        })
+    })
 }
+
+
+
+
+
+
+//---------
+
+// function wantToBuy() {
+//     inquirer
+//         .prompt([
+//             {
+//                 type: "input",
+//                 name: "userChoice",
+//                 message: "What is the ID of the product you would like to buy?",
+//                 validate: function(value) {
+//                     if (isNaN(value) === false) {
+//                         return true;
+//                     }
+//                     return false;
+//                 }
+//             },
+//             {
+//                 type: "input", 
+//                 name: "quantity", 
+//                 message: "How many units would you like to buy",
+//                 validate: function(value) {
+//                     if (isNaN(value) === false) {
+//                         return true;
+//                     }
+//                     return false;
+//                 }
+//             }
+//         ])
+//         .then(function(answer) {
+//             var chosenItem; 
+//             for (var i = 0; i < answer.length; i++) {
+//                 if(answer[i].id === answer.userChoice) {
+//                     chosenItem = answer[i];
+//                 }
+//             }
+//             console.log(chosenItem);
+//         });
+// }
